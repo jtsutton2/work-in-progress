7b08b6f654abfd9666d98c527cd7e8d1e89412ac
jtsutton2 <joshuasutton323@gmail.com>
2024-12-15 11:04:07 -0500

    Create main.ino

---
+++ Arduino/Greenhouse/main.ino
@@ -0,0 +1,9 @@
+import Servo.h
+import RTC.h
+
+/*
+The purpose of this project is to automate
+greenhouse control using moisture sensors,
+temperature sensors, valves, and a real time
+clock (RTC)
+*/
